df455db0d89d219e10d618b9985a08116a392311
nagilanrk14 <nagilanrk14@gmail.com>
2026-05-29 15:35:00 +0530

    Update supabaseClient.js

--- reflex-trainer/supabaseClient.js
+++ reflex-trainer/supabaseClient.js
@@ -2,4 +2,4 @@
 const supabaseUrl = 'https://opxtpghosfcijxrrpngj.supabase.co'; 
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9weHRwZ2hvc2ZjaWp4cnJwbmdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3Nzc3MTQsImV4cCI6MjA5NTM1MzcxNH0.h-X_JT-i_PjRX5wwJN6lwOk9SPI84ZeSJ1THjUY3oa0';
 
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+window.supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
